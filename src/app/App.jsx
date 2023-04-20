544f29254eda7c6ef26ea8a25b3d8e78e4e58520
Cheder-one <cherbadenis1@gmail.com>
2023-04-20 14:47:38 +0300

    `Link to`

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,20 @@
+import Home from "./components/navBar/home";
+import Login from "./components/navBar/login";
 import NavBar from "./components/navBar/navBar";
+import { Route, Switch } from "react-router-dom";
+import Posts from "./components/navBar/posts";
+import Dashboard from "./components/navBar/dashboard";
 
 function App() {
   return (
     <>
       <NavBar />
-      <h1>App</h1>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/posts" component={Posts} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
     </>
   );
 }
