863e0fd9f6fc91e1cbabf372e870501efda326d8
Cheder-one <cherbadenis1@gmail.com>
2023-04-22 14:06:57 +0300

    Хуки react-router-dom

--- src/app/components/navigation/posts.jsx
+++ src/app/components/navigation/posts.jsx
@@ -1,14 +1,18 @@
 import PostList from "../postsList";
 import Post from "../post";
+import { useParams } from "react-router-dom";
 
-const Posts = ({ match, history }) => {
+const Posts = () => {
+  const params = useParams();
   const posts = [
     { id: 1, title: "Post 1" },
     { id: 2, title: "Post 2" },
     { id: 3, title: "Post 3" },
   ];
 
-  const postId = match.params.postId;
+  // const postId = match.params.postId;
+  // const postId = params.postId;
+  const { postId } = params;
 
   return (
     <>
