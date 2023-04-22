76b45e15726fb73a1848f375cda5e749a5c5d2fa
Cheder-one <cherbadenis1@gmail.com>
2023-04-23 01:16:12 +0300

    u

--- src/app/components/navigation/navBar.jsx
+++ src/app/components/navigation/navBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <ul className="nav flex-column">
+    <ul className="nav flex-row">
       <li className="nav-item">
         <Link className="nav-link" to="/">
           Home
